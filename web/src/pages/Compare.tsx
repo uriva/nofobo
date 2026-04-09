2df526ef0a4f38ea592e83bb5dc93a8467186837
Uri <uri.valevski@gmail.com>
2026-04-09 11:16:46 +0300

    fix(ui): wrap mobile compare layout in a react fragment to fix build

--- web/src/pages/Compare.tsx
+++ web/src/pages/Compare.tsx
@@ -324,6 +324,7 @@ export default function Compare() {
             </p>
           </div>
         ) : pair ? (
+          <>
           <div className="flex md:grid md:grid-cols-2 gap-4 md:gap-6 overflow-x-auto md:overflow-visible snap-x snap-mandatory pb-8 pt-2 -mx-6 px-6 md:mx-0 md:px-0 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
             {pair.map((profile, idx) => (
               <div
@@ -369,6 +370,7 @@ export default function Compare() {
             Swipe to compare
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6"/></svg>
           </div>
+          </>
         ) : null}
 
         {/* Skip button */}
